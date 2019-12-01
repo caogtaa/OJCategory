823923f5d5da3f21e157d662fda85ac52a01a595
Cao Gaoting <caogtaa@gmail.com>
2019-12-01 20:50:31 +0800

    support multiple OJ category

--- gen_page.py
+++ gen_page.py
@@ -4,14 +4,21 @@
 import codecs
 import csv
 import os
+import re
 
-def get_suffix(file_path):
+readme_path = './README.md'
+
+def get_full_path(file_path_candidate):
+    if not isinstance(file_path_candidate, list):
+        file_path_candidate = [file_path_candidate]
+
     for suffix in ['.cpp', '.py', '.txt']:
-        if os.path.exists(file_path + suffix):
-            return suffix
+        for file_path in file_path_candidate:
+            if os.path.exists(file_path + suffix):
+                return file_path + suffix
     return ''
 
-def gen_page(oj_config):
+def write_to_readme(oj_name, oj_config, output):
     # oj_name = oj_config['name']
     # problem_prefix = oj_config['url']
 
@@ -20,7 +27,7 @@ def gen_page(oj_config):
     
     records = dict()
     category_map = dict()
-    with open('category.csv', 'r') as input:
+    with open('./%s/category.csv' % oj_name, 'r') as input:
         reader = csv.DictReader(input, delimiter=',')
         for row in reader:
             records[row['id']] = row
@@ -37,47 +44,73 @@ def gen_page(oj_config):
 
     # todo: sort category
     # assign category with specific order
-    with codecs.open('README.md', 'w', 'utf-8') as output:
-        for category in category_map:
-            output.write("### %s\n" % category)
-            counter = 0
-            for id in category_map[category]:
-                if counter > 0 and counter % 10 == 0:
-                    # 10 problems per line
-                    output.write('<br>\n')
-                counter += 1
+    for category in category_map:
+        output.write("### %s\n" % category)
+        counter = 0
+        for id in category_map[category]:
+            if counter > 0 and counter % 10 == 0:
+                # 10 problems per line
+                output.write('<br>\n')
+            counter += 1
 
-                rec = records[id]
-                file_path = file_path_rule(rec)
-                suffix = get_suffix(file_path)
+            rec = records[id]
+            file_path_candidate = file_path_rule(rec)
+            file_path = get_full_path(file_path_candidate)
 
-                if suffix:
-                    output.write('[%s](%s%s)' % ('\U0001F388', file_path, suffix))
-                else:
-                    # file not found, print question emoji
-                    output.write('\U00002754')
-                
-                try:
-                    readable_id = '%04d' % int(id)
-                except:
-                    readable_id = id
+            if file_path:
+                output.write('[%s](%s)' % ('\U0001F388', file_path))
+            else:
+                # file not found, print question emoji
+                output.write('\U00002754')
+            
+            try:
+                readable_id = '%04d' % int(id)
+            except:
+                readable_id = id
 
-                output.write('[%s](%s)' % (readable_id, url_rule(rec)))
-                output.write('&nbsp;&nbsp;&nbsp;&nbsp;')
+            output.write('[%s](%s)' % (readable_id, url_rule(rec)))
+            output.write('&nbsp;&nbsp;&nbsp;&nbsp;')
 
-                # output.write('[%04d](./%s/%d_%d/%04d.py)&nbsp;&nbsp;&nbsp;&nbsp;' % (id, oj_name, from_id, to_id, id))
-            output.write('<br>\n')
+            # output.write('[%04d](./%s/%d_%d/%04d.py)&nbsp;&nbsp;&nbsp;&nbsp;' % (id, oj_name, from_id, to_id, id))
+        output.write('<br>\n')
 
 def PE_file_path_rule(rec):
     id = int(rec['id'])
     folder_id = (id-1) // 100 * 100
     return './%s/%d_%d/%04d' % ('ProjectEuler', folder_id+1, folder_id+100, id)
 
+def HR_url_rule(rec):
+    contest = rec.get('contest')
+    if contest:
+        return 'https://www.hackerrank.com/contests/%s/challenges/%s' % (contest, rec['id'])    
+    return 'https://www.hackerrank.com/challenges/%s/problem' % rec['id'],
+
+pattern = re.compile("['{}*&.?!,…:;]+")
+def HR_file_path_rule(rec):
+    title = rec.get('title')
+    if title:
+        file_name = '_'.join(pattern.sub('', title).split(' '))
+    else:
+        # generate file name from id
+        id = rec.get('id')
+        file_name = '_'.join([token.capitalize() for token in id.split('-')])
+
+    prefix = './HackerRank'
+    contest = rec.get('contest')
+    if contest:
+        prefix += './' + contest
+    
+    return ['%s/HR_%s' % (prefix, file_name), '%s/%s' % (prefix, file_name)]
+
 def get_oj_config(oj_name):
     ojs = {
         'ProjectEuler' : {
             'url_rule': lambda rec: 'https://projecteuler.net/problem=%d' % int(rec['id']),
             'file_path_rule': PE_file_path_rule
+        },
+        'HackerRank' : {
+            'url_rule': HR_url_rule,
+            'file_path_rule': HR_file_path_rule
         }
     }
 
@@ -85,9 +118,18 @@ def get_oj_config(oj_name):
     return ojs.get(oj_name)
 
 def main():
-    oj_names = ['ProjectEuler']
-    for name in oj_names:
-        gen_page(get_oj_config(name))
+    oj_names = ['ProjectEuler', 'HackerRank']
+    with codecs.open(readme_path, 'w', 'utf-8') as output:
+        for name in oj_names:
+            # write header
+            headers = []
+            for name_anchor in oj_names:
+                if name_anchor == name:
+                    headers.append('**%s**' % name_anchor)
+                else:
+                    headers.append('[%s](#%s)' % (name_anchor, name_anchor.lower()))
+            output.write('| %s |\n' % ' | '.join(headers))
+            write_to_readme(name, get_oj_config(name), output)
 
 if __name__ == '__main__':
     main()
