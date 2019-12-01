b45ad58f19ce119992231a4225cd8b9cc3d98c2c
Cao Gaoting <caogtaa@gmail.com>
2019-12-01 21:34:36 +0800

    bugfix

--- gen_page.py
+++ gen_page.py
@@ -5,8 +5,13 @@ import codecs
 import csv
 import os
 import re
+import glob
 
 readme_path = './README.md'
+
+def get_actual_file_name(name):
+    name = "%s[%s]" % (name[:-1], name[-1])
+    return glob.glob(name)[0]
 
 def get_full_path(file_path_candidate):
     if not isinstance(file_path_candidate, list):
@@ -15,7 +20,10 @@ def get_full_path(file_path_candidate):
     for suffix in ['.cpp', '.py', '.txt']:
         for file_path in file_path_candidate:
             if os.path.exists(file_path + suffix):
-                return file_path + suffix
+                file_path_with_suffix = file_path + suffix
+                actual_file_name = get_actual_file_name(file_path_with_suffix)
+                actual_file_name = os.path.basename(actual_file_name)
+                return file_path_with_suffix[:-len(actual_file_name)] + actual_file_name
     return ''
 
 def write_to_readme(oj_name, oj_config, output):
@@ -46,12 +54,12 @@ def write_to_readme(oj_name, oj_config, output):
     # assign category with specific order
     for category in category_map:
         output.write("#### %s\n" % category)
-        counter = 0
+        # counter = 0
         for id in category_map[category]:
-            if counter > 0 and counter % 10 == 0:
-                # 10 problems per line
-                output.write('<br>\n')
-            counter += 1
+            # if counter > 0 and counter % 10 == 0:
+            #     # 10 problems per line
+            #     output.write('<br>\n')
+            # counter += 1
 
             rec = records[id]
             file_path_candidate = file_path_rule(rec)
@@ -74,6 +82,9 @@ def write_to_readme(oj_name, oj_config, output):
             # output.write('[%04d](./%s/%d_%d/%04d.py)&nbsp;&nbsp;&nbsp;&nbsp;' % (id, oj_name, from_id, to_id, id))
         output.write('<br>\n')
 
+    # oj end
+    output.write('<br><br>\n')
+
 def PE_file_path_rule(rec):
     id = int(rec['id'])
     folder_id = (id-1) // 100 * 100
@@ -83,7 +94,7 @@ def HR_url_rule(rec):
     contest = rec.get('contest')
     if contest:
         return 'https://www.hackerrank.com/contests/%s/challenges/%s' % (contest, rec['id'])    
-    return 'https://www.hackerrank.com/challenges/%s/problem' % rec['id'],
+    return 'https://www.hackerrank.com/challenges/%s/problem' % rec['id']
 
 pattern = re.compile("['{}*&.?!,…:;]+")
 def HR_file_path_rule(rec):
@@ -98,7 +109,7 @@ def HR_file_path_rule(rec):
     prefix = './HackerRank'
     contest = rec.get('contest')
     if contest:
-        prefix += './' + contest
+        prefix += '/' + contest
     
     return ['%s/HR_%s' % (prefix, file_name), '%s/%s' % (prefix, file_name)]
 
@@ -128,7 +139,7 @@ def main():
                     headers.append('**%s**' % name)
                 else:
                     headers.append('[%s](#%s)' % (other_name, other_name.lower()))
-            output.write('<a id="%s"></a>| %s |\n' % (name.lower() ,' | '.join(headers)))
+            output.write('### <a id="%s"></a> | %s |\n' % (name.lower() ,' | '.join(headers)))
             write_to_readme(name, get_oj_config(name), output)
 
 if __name__ == '__main__':
