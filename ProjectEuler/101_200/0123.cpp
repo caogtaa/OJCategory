3ce350d8502f48cd6a2d7026c52e68cac41f35f0
caogtaa <caogtaa@gmail.com>
2019-12-31 05:04:54 +0800

    finish 0123

---
+++ ProjectEuler/101_200/0123.cpp
@@ -0,0 +1,44 @@
+
+#include <common_headers.h>
+#include <helper.h>
+
+using namespace std;
+
+class Solution {
+public:
+	long long solve() {
+		PrimeHelper helper;
+		helper.init(1000000);
+		auto primes = helper.getPrimes(2, 1000000);
+
+		long long n;
+		long long pn;
+		long long sum;
+		long long lmt = (long long)1e10;
+		for (int i = 0; i < primes.size(); ++i) {
+			n = i+1;
+			pn = primes[i];
+			if (n & 1)
+				sum = pn * n * 2;
+			else
+				sum = 2;
+
+			if (sum % (pn * pn) > lmt)
+				return n;
+		}
+
+		return -1;
+	}
+};
+
+int main() {
+	auto s = new Solution();
+	cout << s->solve() << endl;
+	delete s;
+	return 0;
+}
+
+// https://projecteuler.net/problem=123
+// 数论分析
+// 二项式展开
+// https://www.yuque.com/caomou/mgxqtg/eagl0b
